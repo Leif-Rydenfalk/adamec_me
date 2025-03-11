9fd7f85d4c6e3392f2c373b830b4b366ac08e1b8
Kharif <rydenfalk.service@gmail.com>
2025-03-11 12:36:58 +0100

    s

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+
+	import { i18n } from '$lib/i18n';
+	import { ModeWatcher } from 'mode-watcher';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import Header from '$components/header.svelte';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
